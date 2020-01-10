2b9e353e1fd8b4dd176a8d8048a80fe72f226849
echosoar <82163514@qq.com>
2020-01-11 01:19:02 +0800

    feat: support more lang

--- week/lang.go
+++ week/lang.go
@@ -4,20 +4,34 @@ import (
 )
 
 var langMap = map[string]string {
-	"ts": "TypeScript",
+	"c": "C",
+	"cpp": "C++",
+	"css": "CSS",
+	"go": "Go",
+	"h": "C",
+	"html": "HTML",
+	"java": "Java",
 	"js": "JavaScript",
+	"jsx": "JavaScript",
 	"json": "Json",
+	"makefile": "Makefile",
+	"md": "Markdown",
+	"php": "PHP",
+	"py": "Python",
+	"sh": "Shell",
+	"ts": "TypeScript",
+	"tsx": "TypeScript",
 	"yml": "Yaml",
-	"go": "Golang",
 }
 
 func GetFileType(allFileName string) string {
 	fileNameList := strings.Split(allFileName, "/")
 	fileName := fileNameList[len(fileNameList) - 1]
 	fileNameExtList := strings.Split(fileName, ".")
-	fileNameExt := langMap[fileNameExtList[len(fileNameExtList) - 1]]
+	fileNameExtStr := fileNameExtList[len(fileNameExtList) - 1]
+	fileNameExt := langMap[strings.ToLower(fileNameExtStr)]
 	if fileNameExt != "" {
 		return fileNameExt
 	}
-	return "unknow"
+	return "Other"
 }
